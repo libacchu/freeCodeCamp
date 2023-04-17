2419d174724356e05deb540baff379b4533ad035
libacchu <libacchu@students.42wolfsburg.de>
2023-04-17 20:26:47 +0200

    time_calculator

---
+++ Scientific Computing with Python Projects/1. Arithmetic Formatter/2. Time Calculator/time_calculator.py
@@ -0,0 +1,20 @@
+import math
+
+def add_time(start, duration, day = None):
+    # Split the start time into hours, minutes, and period 
+
+    # Split the duration into hours and minutes
+
+    # Calculate the new time
+
+    # Calculate the new period
+    
+    # Calculate the number of days later
+    
+    # Format the new time  
+
+    # Add the day of the week if it was provided
+
+    
+
+    return new_time
